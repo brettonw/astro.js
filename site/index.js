1b55abe030d6430bd6686ce4669523a9f7b8fe89
Bretton Wade <brettonw.wade@gmail.com>
2016-11-02 11:12:58 -0400

    orbit cameras work again checkpoint on moving solar system computations into a central control point

--- site/index.js
+++ site/index.js
@@ -2,15 +2,13 @@
 
 let scene;
 
-let currentPosition = [0, 0];
-
 let standardUniforms = Object.create(null);
 
 let showStarsCheckbox;
 let showConstellationsCheckbox;
 let showCloudsCheckbox;
 let showAtmosphereCheckbox;
-let fovRange;
+let zoomRange;
 let framingRange;
 let timeRange;
 let dayRange;
@@ -21,6 +19,8 @@ let cloudsNode;
 let atmosphereNode;
 
 let timeDisplay;
+
+let solarSystem = Object.create (null);
 
 let scaleRange = function (range, deadZone) {
     let rangeValue = range.value;
@@ -38,7 +38,15 @@ let geo_2016_11_1_1200 = computeJ2000 (utc (2016, 11, 1, 18, 0, 0));
 let eclipse2017 = computeJ2000 (utc (2017, 8, 21, 18, 0, 0));
 let dscovrMoonTransit2015 = computeJ2000 (utc (2015, 7, 16, 0, 0, 0));
 
+let refreshTimeoutId = 0;
 let draw = function (deltaPosition) {
+    // ensure the draw function is called about once per minute to keep the display refreshed
+    if (refreshTimeoutId != 0) {
+        clearTimeout (refreshTimeoutId);
+    }
+    setTimeout(function () { draw ([0, 0]); }, 1000 * 60);
+
+    // determine how to set the clock
     let timeType = document.getElementById ("timeTypeSelect").value;
     switch (timeType) {
         case "paused": break;
@@ -50,41 +58,22 @@ let draw = function (deltaPosition) {
     let hourDelta = scaleRange(timeRange, 0.025) * 2.0;
     let dayDelta = scaleRange(dayRange, 0.025) * 180.0;
     let displayTime = currentTime + dayDelta + hourDelta;
+    updateSolarSystem (displayTime);
     Thing.updateAll(displayTime);
 
     // XXX convert from our display time to a Javascript Date
-    timeDisplay.innerHTML = "JD: " + displayTime.toFixed(4);
-
-    // update the current position and clamp or wrap accordingly
-    currentPosition = Float2.add (currentPosition, deltaPosition);
-    while (currentPosition[0] > 1) {
-        currentPosition[0] -= 2;
-    }
-    while (currentPosition[0] < -1) {
-        currentPosition[0] += 2;
-    }
-    currentPosition[1] = Math.min (currentPosition[1], 0.9);
-    currentPosition[1] = Math.max (currentPosition[1], -0.9);
+    timeDisplay.innerHTML = "JD: " + displayTime.toFixed(4) + " (" + currentTime.toFixed (4) + ", " + (dayDelta + hourDelta).toFixed (4) + ")";
 
     // set up the projection matrix (earth radius is 1 and we want it to occupy about 75% of the
     // view in the vertical direction - the view is probably wider than that)
-    let fovRangeValue = fovRange.value;
-    fovRangeValue *= 0.01;
-    fovRangeValue *= fovRangeValue;
-    fovRangeValue = 1.0 - fovRangeValue;
-    starsNode.alpha = fovRangeValue;
-    fovRangeValue = 0.5 + (59.5 * fovRangeValue);
-    let fov = fovRangeValue;
-/*
-    let framingRangeValue = framingRange.value;
-    framingRangeValue *= 0.01;
-    framingRangeValue = 0.1 + (0.9 * framingRangeValue);
+    let zoomRangeValue = zoomRange.value;
+    zoomRangeValue *= 0.01;
+    zoomRangeValue *= zoomRangeValue;
 
-    let halfFov = fov / 2.0;
-    let goalOpposite = 1.0 / framingRangeValue;
-    let sinTheta = Math.sin(Utility.degreesToRadians (halfFov));
-    let hypotenuse = goalOpposite / sinTheta;
-    */
+    let zoomRangeValueInverse = 1.0 - zoomRangeValue;
+    starsNode.alpha = zoomRangeValueInverse;
+    let fov = 0.5 + (59.5 * zoomRangeValueInverse);
+
     //console.log("Setting Projection at: " + hypotenuse);
     // I'm cheating with the near/far, I know the moon and anything orbiting it is the farthest out
     // we'll want to see on the near side, and the starfield on the far side
@@ -93,34 +82,66 @@ let draw = function (deltaPosition) {
     let farPlane = hypotenuse + 211.0;
     */
     let nearPlane = 0.1;
-    let farPlane = 620;
+    let farPlane = 440;
     standardUniforms.PROJECTION_MATRIX_PARAMETER = Float4x4.perspective (fov, context.viewportWidth / context.viewportHeight, nearPlane, farPlane);
 
     let cameraSelect = document.getElementById ("cameraSelect").value;
     let cameraSelectSplit = cameraSelect.split(";");
     let cameraFrom = cameraSelectSplit[0];
+    let lookFromNode = Node.get (cameraFrom);
     let cameraTo = cameraSelectSplit[1];
+    let lookAtNode = Node.get (cameraTo);
     let cameraUp = Utility.defaultValue (cameraSelectSplit[2], "y-up");
 
-    let rootNode = Node.get ("root");
-
-    // get the sun node, run a point through the transformation
-    let lookFromNode = Node.get (cameraFrom);
-    let lookFromTransform = lookFromNode.getTransform (rootNode);
-    let lookFromPoint = Float4x4.preMultiply ([0, 0, 0, 1], lookFromTransform);
-    //console.log ("LOOK FROM: " + Float3.str (lookFromPoint));
-
-    // get the earth node, run a point through the transformation
-    let lookAtNode = Node.get (cameraTo);
-    let lookAtTransform = lookAtNode.getTransform (rootNode);
+    // run a point through the "to" node transformation
+    let lookAtTransform = lookAtNode.getTransform ();
     let lookAtPoint = Float4x4.preMultiply ([0, 0, 0, 1], lookAtTransform);
     //console.log ("LOOK AT: " + Float3.str (lookAtPoint));
+
+    // update the orbit camera if we should
+    if (cameraFrom == "camera") {
+        if (!(cameraTo in lookFromNode.currentPosition)) {
+            lookFromNode.currentPosition[cameraTo] = [0, 0];
+        }
+
+        // update the current controller position and clamp or wrap accordingly
+        let currentPosition = Float2.add (lookFromNode.currentPosition[cameraTo], deltaPosition);
+        currentPosition[0] = Utility.unwind (currentPosition[0], 2);
+        currentPosition[1] = Math.max (Math.min (currentPosition[1], 0.9), -0.9);
+        lookFromNode.currentPosition[cameraTo] = currentPosition;
+
+        // compute a few image composition values based off ensuring a sphere is fully in view
+        // XXX for now, we assume the bound on the observed object is 1, but I need to get bounds
+        // XXX on the nodes to be really effective
+        let boundRadius = 1.0;
+        let goalOpposite = boundRadius / zoomRangeValue;
+        let sinTheta = Utility.sin (fov / 2.0);
+        let hypotenuse = goalOpposite / sinTheta;
+
+        // setup the transformation matrix
+        let cameraMatrix = Float4x4.chain (
+            Float4x4.translate ([hypotenuse, 0, 0]),
+            Float4x4.rotateZ (currentPosition[1] * Math.PI * 0.5),
+            Float4x4.rotateY (currentPosition[0] * Math.PI * -1),
+            Float4x4.translate (lookAtPoint)
+        );
+
+        // apply it
+        // XXX this probably needs to either incorporate the "to" node transform, or it needs to be
+        // XXX a relocated node to be a child of the "to"
+        lookFromNode.transform = cameraMatrix;
+    }
+
+    // run a point through the "from" nodetransformation
+    let lookFromTransform = lookFromNode.getTransform ();
+    let lookFromPoint = Float4x4.preMultiply ([0, 0, 0, 1], lookFromTransform);
+    //console.log ("LOOK FROM: " + Float3.str (lookFromPoint));
 
     // figure the up vector
     let up = [0, 1, 0];
     if (cameraUp != "y-up") {
         let upNode = Node.get (cameraUp);
-        let upTransform = upNode.getTransform (rootNode);
+        let upTransform = upNode.getTransform ();
         let upPoint = Float4x4.preMultiply ([0, 0, 0, 1], upTransform);
         up = Float3.normalize (Float3.subtract (upPoint, lookFromPoint));
     }
@@ -129,21 +150,6 @@ let draw = function (deltaPosition) {
     let viewMatrix = Float4x4.lookFromAt (lookFromPoint, lookAtPoint, up);
     //let viewMatrix = Float4x4.lookAlongAt (3, 50, Float3.subtract (lookAtPoint, lookFromPoint), lookAtPoint);
 
-    // compute the view parameters as up or down, and left or right
-    /*
-    let upAngle = currentPosition[1] * Math.PI * 0.5;
-    let viewOffset = Float2.scale ([Math.cos (upAngle), Math.sin (upAngle)], -hypotenuse);
-    */
-
-    // setup the view matrix
-    /*
-    let viewMatrix = Float4x4.identity ();
-    viewMatrix = Float4x4.multiply (Float4x4.rotateX (upAngle), viewMatrix);
-    viewMatrix  = Float4x4.multiply (Float4x4.translate ([0, viewOffset[1], viewOffset[0]]), viewMatrix);
-    viewMatrix = Float4x4.multiply (Float4x4.rotateY (currentPosition[0] * Math.PI), viewMatrix);
-    //viewMatrix = Float4x4.multiply (Float4x4.scale ([ 2, 2, 2 ]), viewMatrix);
-    //viewMatrix  = Float4x4.multiply (Float4x4.translate ([ -0.5, -0.5, -0.5 ]), viewMatrix);
-    */
     standardUniforms.VIEW_MATRIX_PARAMETER = viewMatrix;
     standardUniforms.MODEL_MATRIX_PARAMETER = Float4x4.identity ();
 
@@ -166,8 +172,52 @@ let selectCamera = function () {
     let cameraSelect = document.getElementById ("cameraSelect").value;
     let cameraSelectSplit = cameraSelect.split (";");
     let cameraFov = Utility.defaultValue (cameraSelectSplit[3], 0.5);
-    fovRange.value = fovRange.max * cameraFov;
+    zoomRange.value = zoomRange.max * cameraFov;
     draw ([0, 0]);
+};
+
+let updateSolarSystem = function (time) {
+    // cos and sin routines that work on degrees (unwraps intrinsically)
+    let cos = Utility.cos;
+    let sin = Utility.sin;
+
+    // compute the julian century
+    let jc = time / 36525;
+
+    // SUN
+    {
+        // compute the mean longitude and mean anomaly of the sun (degrees)
+        let meanLongitude = 280.460 + (36000.77 * jc);
+        let meanAnomaly = 357.5277233 + (35999.05034 * jc);
+
+        // compute the ecliptic longitude of the sun (degrees)
+        let eclipticLongitude = meanLongitude + (1.914666471 * sin (meanAnomaly)) + (0.019994643 * sin (meanAnomaly + meanAnomaly));
+
+        // compute the distance to the sun in astronomical units
+        solarSystem.sunR = 1.000140612 - (0.016708617 * cos (meanAnomaly)) - (0.000139589 * cos (meanAnomaly + meanAnomaly));
+
+        // compute the ecliptic obliquity (degrees)
+        let eclipticObliquity = 23.439291 - (0.0130042 * jc);
+
+        // compute geocentric equatorial coordinates, note that these are re-ordered to reflect the
+        // rotation of the solar system coordinate frame into my Y-up viewing frame
+        let sinEclipticLongitude = sin (eclipticLongitude);
+        let I = cos (eclipticLongitude);
+        let J = cos (eclipticObliquity) * sinEclipticLongitude;
+        let K = sin (eclipticObliquity) * sinEclipticLongitude;
+
+        solarSystem.sunDirection = Float3.normalize ([-I, K, J]);
+    }
+
+    // MOON
+    {
+
+    }
+
+    // L1
+    {
+
+    }
 };
 
 let buildScene = function () {
@@ -212,6 +262,7 @@ let buildScene = function () {
         transform: Float4x4.translate([0.0, 0.0, 8.0]),
         children: false
     });
+    cameraNode.currentPosition = Object.create (null);
     scene.addChild (cameraNode);
 
     let starsTransform = Float4x4.identity ();
@@ -219,7 +270,7 @@ let buildScene = function () {
     // degrees to orient correctly. then flip it inside out and scale it up
     starsTransform = Float4x4.multiply (Float4x4.rotateX (Math.PI), starsTransform);
     starsTransform = Float4x4.multiply (Float4x4.rotateY (Math.PI), starsTransform);
-    starsTransform = Float4x4.multiply (Float4x4.scale (-310), starsTransform);
+    starsTransform = Float4x4.multiply (Float4x4.scale (-220), starsTransform);
     starsNode = Node.new ({
         name: "stars",
         transform: starsTransform,
@@ -258,13 +309,15 @@ let buildScene = function () {
     starsNode.addChild (constellationsNode);
 
     // radii in km so I can do some reasoning about scales...
-    let earthRadius = 6378.1370;
-    let sunRadius = 695700.0;
-    let earthOrbit = 149597870.700;
+    const earthRadius = 6378.1370;
+    const sunRadius = 695700.0;
+    const earthOrbit = 149597870.700;
 
-    let sunDrawDistance = 300;
-    let sunDistance = earthOrbit / earthRadius;
-    let sunScale = (sunRadius / earthRadius);
+    const sunDistance = earthOrbit / earthRadius;
+
+    // compute the drawDistance so that the actual sun scale is usually about 1
+    const sunDrawDistance = (sunDistance * earthRadius) / sunRadius;
+    console.log ("Sun draw distance = " + sunDrawDistance);
     let sunNode = Node.new ({
         name: "sun",
         transform: Float4x4.identity (),
@@ -282,49 +335,19 @@ let buildScene = function () {
         // get the node
         let node = Node.get (this.node);
 
-        // cos and sin routines that work on degrees (unwraps intrinsically)
-        let cos = Utility.cos;
-        let sin = Utility.sin;
-
-        // compute the julian century
-        let jc = time / 36525;
-
-        // compute the mean longitude and mean anomaly of the sun (degrees)
-        let meanLongitude = 280.460 + (36000.77 * jc);
-        let meanAnomaly = 357.5277233 + (35999.05034 * jc);
-
-        // compute the ecliptic longitude of the sun (degrees)
-        let eclipticLongitude = meanLongitude + (1.914666471 * sin (meanAnomaly)) + (0.019994643 * sin (meanAnomaly + meanAnomaly));
-
-        // compute the distance to the sun in astronomical units
-        let R = 1.000140612 - (0.016708617 * cos (meanAnomaly)) - (0.000139589 * cos (meanAnomaly + meanAnomaly));
-
-        // compute the ecliptic obliquity (degrees)
-        let eclipticObliquity = 23.439291 - (0.0130042 * jc);
-
-        // compute geocentric equatorial coordinates
-        let sinEclipticLongitude = sin (eclipticLongitude);
-        let I = R * cos (eclipticLongitude);
-        let J = R * cos (eclipticObliquity) * sinEclipticLongitude;
-        let K = R * sin (eclipticObliquity) * sinEclipticLongitude;
-
-        // I am using a right handed coordinate system where X is positive to the left, Y positive
-        // up, and Z positive into the view
-        let sunDirection = Float3.normalize ([-I, K, J]);
-        //console.log ("SUN: " + Float3.str (sunDirection));
-        let sunPosition = Float3.scale (sunDirection, sunDrawDistance);
+        let sunPosition = Float3.scale (solarSystem.sunDirection, sunDrawDistance);
 
         // compute the relative scale of the sun to reflect the changing distance in our orbit
-        sunScale = (sunRadius / earthRadius) * (sunDrawDistance / (sunDistance * R)); // approx 0.93
+        let sunScale = (sunRadius / earthRadius) * (sunDrawDistance / (sunDistance * solarSystem.sunR)); // approx 1
 
         // compute the position of the sun, and update the lighting direction
         node.transform = Float4x4.multiply (Float4x4.scale (sunScale), Float4x4.translate (sunPosition));
-        standardUniforms.LIGHT_DIRECTION = sunDirection;
+        standardUniforms.LIGHT_DIRECTION = solarSystem.sunDirection;
     });
 
-    let moonRadius = 1737.1;
+    const moonRadius = 1737.1;
     //let moonOrbit = 384405.0;
-    let moonScale = moonRadius / earthRadius; // approx 0.273
+    const moonScale = moonRadius / earthRadius; // approx 0.273
     let moonNode = Node.new ({
         name: "moon",
         transform: Float4x4.identity (),
@@ -623,7 +646,7 @@ let onBodyLoad = function () {
     showConstellationsCheckbox = document.getElementById ("showConstellationsCheckbox");
     showCloudsCheckbox = document.getElementById("showCloudsCheckbox");
     showAtmosphereCheckbox = document.getElementById("showAtmosphereCheckbox");
-    fovRange = document.getElementById("fovRange");
+    zoomRange = document.getElementById("zoomRange");
     framingRange = document.getElementById("framingRange");
     timeRange = document.getElementById ("timeRange");
     dayRange = document.getElementById ("dayRange");
